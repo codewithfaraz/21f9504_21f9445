39c8f3f14f2159ff52055e26952f19031fca0ce5
test <test@IT-Lab5-V48.NUCFD.edu.local>
2023-10-06 10:19:16 +0500

    new multiply function

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,18 @@
 
 public class Calculator {
+	public double add(double a, double b) {
+		return a + b;
+	}
 
+	public double subtract(double a, double b) {
+		return a - b;
+	}
+
+	public double multiply(double a, double b) {
+		return a * b;
+	}
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+	}
 }
